20d4744a6e5feaede5a1e26a46ff087a0f5027ed
phalvesr <phalvesrnd@gmail.com>
2020-04-21 17:32:55 -0300

    Changes to align HTML using CSS and JS

--- functions.js
+++ functions.js
@@ -2,6 +2,10 @@ const addP1 = document.getElementById('player1Plus');
 const subP1 = document.getElementById('player1Minus');
 const addP2 = document.getElementById('player2Plus');
 const subP2 = document.getElementById('player2Minus');
+const content = document.getElementById('contentScoreboard')
+
+window.addEventListener('load', setScroreboardPosition);
+window.addEventListener('resize', setScroreboardPosition);
 
 addP1.addEventListener('click', add);
 addP2.addEventListener('click', add);
@@ -14,4 +18,13 @@ function add() {
 
 function sub() {
     alert('Subed!');
+}
+
+function setScroreboardPosition() {
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+
+    content.style.position = 'absolute';
+    content.style.left = ((width/2) - 235) + 'px';
+    content.style.top = ((height/2) - 20) + 'px';
 }
